55f0872df0bff827fa7c70c774e6a94b430d824d
remigamon <arh75472@gmail.com>
2025-06-21 18:27:22 +0800

    Update mobile_nav.js

--- Final_Project_Updated/mobile_nav.js
+++ Final_Project_Updated/mobile_nav.js
@@ -2,3 +2,25 @@
       const navMenu = document.getElementById('navMenu');
       navMenu.classList.toggle('show');
     });
+  const images = [
+    'url("pictures/images1.jpg")',
+    'url("pictures/images2.jpg")',
+      'url("pictures/images3.jpg")',
+      'url("pictures/images4.jpg")',
+      'url("pictures/images5.jpg")',
+      'url("pictures/images6.jpg")',
+      'url("pictures/images7.jpg")',
+      'url("pictures/images8.jpg")',
+      'url("pictures/images9.jpg")',
+    'url("pictures/images10.jpg")'
+  ];
+
+  let index = 0;
+
+  function changeBackground() {
+    document.body.style.backgroundImage = images[index];
+    index = (index + 1) % images.length;
+  }
+
+  setInterval(changeBackground, 5000); // Change every 5 seconds
+  changeBackground(); // Initial call
